2a2a6c31c88e659f5c709e0a6a95e13d97948423
Sangyeon Kim <SangyeonKim@student.unsw.edu.au>
2025-11-10 12:16:28 +1100

    added route and component for EditListing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import HostedListings from "./pages/HostedListings";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/hosted-listings" element={<HostedListings />} />
       <Route path="/listings/new" element={<CreateListing />} />
+      <Route path="/listings/:listingId/edit" element={<EditListing />} />
     </Routes>
   );
 };
